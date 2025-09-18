3637c73ae15623408c2d9b80e399b94fd45903f6
[dyad] <stellardreamzshop@gmail.com>
2025-09-18 11:22:54 -0300

    [dyad] Making a minor text change to trigger deployment - wrote 1 file(s)

--- src/components/Fizz.tsx
+++ src/components/Fizz.tsx
@@ -1,8 +1,8 @@
 export const Fizz = () => {
   return (
     <div className="p-4 text-center">
-      <p className="text-sm text-gray-500 dark:text-gray-400">
-        Random sh*t - Boxxed Apps
+      <p className="text-sm text-white/70">
+        Hecho con ❤️ por Boxxed Apps
       </p>
     </div>
   );
