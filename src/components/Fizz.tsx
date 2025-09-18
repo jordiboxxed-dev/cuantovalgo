8d8f77d3e31aad16f588b9b7a170092cb22f5050
[dyad] <stellardreamzshop@gmail.com>
2025-09-18 11:08:04 -0300

    [dyad] Updating footer text - wrote 1 file(s)

--- src/components/Fizz.tsx
+++ src/components/Fizz.tsx
@@ -2,7 +2,7 @@ export const Fizz = () => {
   return (
     <div className="p-4 text-center">
       <p className="text-sm text-gray-500 dark:text-gray-400">
-        fizz
+        Random sh*t - Boxxed Apps
       </p>
     </div>
   );
